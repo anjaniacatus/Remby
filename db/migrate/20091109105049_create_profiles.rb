1e04e52f2275b1f654d12137695a994de12ef430
root <root@fenomamy.(none)>
2009-11-09 17:31:26 +0300

    account created

---
+++ db/migrate/20091109105049_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration
+  def self.up
+    create_table :profiles do |t|
+      t.string :family_name
+      t.string :name
+      t.string :town
+      t.string :sex
+      t.date :birthday
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :profiles
+  end
+end
