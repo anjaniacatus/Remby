f98480cefe7f010034223d0d0acf0706a0515893
catus <catus@yahoo.fr>
2009-11-10 15:27:05 +0300

    account resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,22 +6,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:notice] = "Successfully registration."
-      redirect_to users_url 
+      flash[:notice] = "Successfully Registered."
+      redirect_to root_url 
     else
       render :action => 'new'
     end
   end
   
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
   
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     if @user.update_attributes(params[:user])
-      flash[:notice] = "Successfully updated user."
-      redirect_to users_url
+      flash[:notice] = "Successfully updated account."
+      redirect_to root_url
     else
       render :action => 'edit'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :offerings
 
   map.login "login", :controller => "user_sessions", :action => "new"
-  #map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
   map.resources :user_sessions
-
+  map.resources :offerings
   map.resources :users
-
   map.resources :profiles
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -42,7 +40,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-   map.root :controller => "profiles"
+   map.root :controller => "offerings"
 
   # See how all your routes lay out with "rake routes"
 
