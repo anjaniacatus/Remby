c5b1b16fe9491842a3bb0fa26728e6d348b5f448
catus <catus@yahoo.fr>
2009-11-16 09:49:48 +0300

    validation added

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,4 @@
 class Experience < ActiveRecord::Base
   belongs_to :profile
+  validate_presence_of :permalink, :office_name,:society_name, :period
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,4 @@
 class Profile < ActiveRecord::Base
+  has_many :experiences 
+  validates_presence_of :firstname, :lastname, :permalink, :age, :sex
 end
