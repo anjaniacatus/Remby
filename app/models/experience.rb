43493388f479cf6906f5a0aac299eb16ceaafe1c
root <root@fenomamy.(none)>
2009-11-23 09:09:48 +0300

    add nested form

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,4 @@
 class Experience < ActiveRecord::Base
   belongs_to :profile
-  validate_presence_of :permalink, :office_name,:society_name, :period
+  validates_presence_of :permalink, :office_name,:society_name, :period
 end
