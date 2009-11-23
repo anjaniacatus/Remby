5a6b897cb07253711d888378ec75df4eb6d60b20
catus <catus@yahoo.fr>
2009-11-23 08:03:10 +0300

    edit view of profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,14 @@
 class Profile < ActiveRecord::Base
-  has_many :experiences
   has_many :education_informations
   accepts_nested_attributes_for :education_informations, :allow_destroy => true
+  has_many :experiences
   accepts_nested_attributes_for :experiences, :allow_destroy => true
+   has_many :skills
+  accepts_nested_attributes_for :skills, :allow_destroy => true
+  has_many :other_infos
+  accepts_nested_attributes_for :other_infos, :allow_destroy => true
+  
+ 
+
   validates_presence_of :firstname, :lastname, :permalink, :age, :sex
 end
