8ef03dd93be780e3f1c0ff8cad3bdc2ee4e96802
catus <catus@yahoo.fr>
2010-03-01 14:04:22 +0300

    relation added to compagny

--- app/models/compagny.rb
+++ app/models/compagny.rb
@@ -1,3 +1,4 @@
 class Compagny < ActiveRecord::Base
   validates_presence_of :name, :registration_number, :headquaters   
+  has_many :job_sheets
 end
--- app/models/job_sheet.rb
+++ app/models/job_sheet.rb
@@ -1,3 +1,4 @@
 class JobSheet < ActiveRecord::Base
   validates_presence_of :position, :mission, :competence, :fields
+  belongs_to :compagny
 end
