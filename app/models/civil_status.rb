0dd921d456a77ff320a1875a3588c0acdb805aea
catus <catus@fenoum.(none)>
2010-04-21 13:08:20 +0300

    qualifiable removed

--- app/models/civil_status.rb
+++ app/models/civil_status.rb
@@ -1,7 +1,6 @@
 class CivilStatus < ActiveRecord::Base
   validates_presence_of :family_name, :name, :gender, :age, :current_city
   acts_as_contactable
-  acts_as_qualifiable
   has_friendly_id :name, :use_slug => true, :strip_diacritics => true
   Genders = ["","femme", "homme"]
   Statuses = ["","marié(e)","celibataire"]
