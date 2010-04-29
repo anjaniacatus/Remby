2b5f67b7aec754826fd5f28d2a1ec8e31a8e2be9
catus <catus@fenoum.(none)>
2010-04-29 09:50:26 +0300

    field added to degree_course

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100428153322) do
+ActiveRecord::Schema.define(:version => 20100429064818) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -102,6 +102,13 @@ ActiveRecord::Schema.define(:version => 20100428153322) do
     t.string   "jobtitle"
     t.integer  "job_id"
     t.string   "duration"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fields", :force => true do |t|
+    t.string   "field_name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
