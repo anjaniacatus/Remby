98249457647b3bc3da4f26ccf39656d70c2361b7
catus <catus@fenoum.(none)>
2010-05-27 08:29:38 +0300

    cvs form updated

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,6 @@
 class Experience < ActiveRecord::Base
   belongs_to :cv
+  has_one :compagny
   validates_presence_of :jobtitle 
   Durations = ["moins d'un ans","1 à 2 ans", "moins de 5ans", "5 ans et plus" ]
   has_friendly_id :jobtitle, :use_slug => true, :strip_diacritics => true
