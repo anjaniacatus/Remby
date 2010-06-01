251f47b47fea30db51c76295807c9a4acb40615c
catus <catus@fenoum.(none)>
2010-06-01 16:18:59 +0300

    search added

---
+++ db/migrate/20100601120336_create_searches.rb
@@ -0,0 +1,16 @@
+class CreateSearches < ActiveRecord::Migration
+  def self.up
+    create_table :searches do |t|
+      t.integer :field_id
+      t.string :keywords
+      t.date :after
+      t.date :before
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :searches
+  end
+end
