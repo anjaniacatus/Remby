670c4cc9825d25ac1cd4346ea0fad2f1210f533e
catus <catus@fenoum.(none)>
2010-06-04 09:57:55 +0300

    link job added

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-   map.root :controller => "cvs"
+   map.root :controller => "jobs"
 
   # See how all your routes lay out with "rake routes"
 
