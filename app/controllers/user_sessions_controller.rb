c89e25404273f113bbcead925f8514a6f82ce482
catus <catus@fenoum.(none)>
2010-09-24 10:06:45 +0300

    flash notice updated

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,6 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
+      flash[:notice] = "Successfully logged in"
       @user = @user_session.user
       unless @user.roles == "admin"
        
